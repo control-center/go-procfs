7051ded6702ae2846b20f7f6b79dba2a63ed79dc
Daniel Garcia <daniel@danielgarcia.info>
2014-10-08 09:17:17 -0500

    add DirectMap1G, ignore unknown fields silently

--- linux/meminfo.go
+++ linux/meminfo.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -53,6 +52,7 @@ type Meminfo struct {
 	Hugepagesize      uint64
 	DirectMap4k       uint64
 	DirectMap2M       uint64
+	DirectMap1G       uint64
 }
 
 var procMeminfo = "/proc/meminfo"
@@ -184,8 +184,9 @@ func ReadMeminfo() (meminfo Meminfo, err error) {
 			meminfo.DirectMap4k = val
 		case "DirectMap2M":
 			meminfo.DirectMap2M = val
+		case "DirectMap1G":
+			meminfo.DirectMap1G = val
 		default:
-			log.Printf("ignoring unknown meminfo field %s", name)
 		}
 	}
 	return
